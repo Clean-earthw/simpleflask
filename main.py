54c4a33953622b88de59f3bb4297eb728cecdd72
Clean-earthw <Tonynesh9@gmail.com>
2024-11-11 07:11:44 -0800

    Update main.py

--- main.py
+++ main.py
@@ -94,11 +94,13 @@ def hello_world():
     name = os.environ.get("NAME", "World")
     return f"Hello {name}!"
         
-@app.before_request 
+@app.before_request
 def before_request():
-     headers = { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS', 'Access-Control-Allow-Headers': 'Content-Type' }
-     if request.method == 'OPTIONS' or request.method == 'options': 
-         return jsonify(headers), 200        
+    headers = {'Access-Control-Allow-Origin': '*',
+               'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+               'Access-Control-Allow-Headers': 'Content-Type'}
+    if request.method.lower() == 'options':
+        return jsonify(headers), 200     
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
