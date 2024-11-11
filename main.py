49168c1deb06c341e7901d74898119dce383c635
Clean-earthw <Tonynesh9@gmail.com>
2024-11-11 03:42:52 -0800

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ genai.configure(
 
 app = Flask(__name__)
 app.secret_key = 'XT5PUdwqegbndhgfsbdvH5m79D'
-CORS(app)
+CORS(app, resources=r'/*', headers='Content-Type')
 app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
